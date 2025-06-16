286d51e2cca4f1329b1c6b88a97e31b5a4aae733
Zain Ul Abidin <ulabidinz1@gmail.com>
2025-06-16 14:33:39 +0500

    feat: implement user registration endpoint with async error handling

--- src/app.js
+++ src/app.js
@@ -18,4 +18,9 @@ app.use(express.urlencoded({
 app.use(express.static("public"));
 app.use(cookieParser());
 
+
+// Routes
+import userRouter from "./routes/user.routes.js";
+app.use("/api/v1/users", userRouter);
+
 export default app;
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,14 +1,21 @@
-const asyncHandler = async (requestHandler) => {
+const asyncHandler = (requestHandler) => {
     return async (req, res, next) => {
-        try {
-           await requestHandler(req, res, next);
-        } catch (error) {
-            res.status(error.statusCode || 500).json({
-                success: false,
-                message: error.message || "Something went wrong",
-            });
-        }
+        Promise.resolve(requestHandler(req, res, next))
+            .catch((error) => next(error));
     }
 }
+
+// const asyncHandler = async (requestHandler) => {
+//     return async (req, res, next) => {
+//         try {
+//            await requestHandler(req, res, next);
+//         } catch (error) {
+//             res.status(error.statusCode || 500).json({
+//                 success: false,
+//                 message: error.message || "Something went wrong",
+//             });
+//         }
+//     }
+// }
 
 export default asyncHandler;
